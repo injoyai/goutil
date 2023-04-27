64476451c549e681250c4f85b6ef3cd3602024e6
钱纯净 <1113655791@qq.com>
2023-04-27 22:17:16 +0800

    优化bar,进度条

---
+++ string/bar/bar_test.go
@@ -0,0 +1,30 @@
+package bar
+
+import (
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	x := New()
+	x.SetPrefix("进度:")
+	x.SetWidth(50)
+	x.SetTotalSize(1100)
+	//x.SetColor(color.Violet + 10)
+	x.SetStyle(">")
+	go func() {
+		for i := 0; i < 1000; i++ {
+			time.Sleep(time.Millisecond * 100)
+			x.Add(10)
+		}
+	}()
+	go func() {
+		time.Sleep(time.Second * 3)
+		//x.Done()
+	}()
+	<-x.Wait()
+}
+
+func TestDemo(t *testing.T) {
+	Demo()
+}
