e4e9d7585f2fe2273243a3cd8e48b679174ead8c
钱纯净 <1113655791@qq.com>
2023-07-04 23:54:32 +0800

    优化bar,待优化网速算法

--- string/bar/testdata/main.go
+++ string/bar/testdata/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"github.com/injoyai/goutil/string/bar"
+	"github.com/injoyai/logs"
 )
+
+func init() {
+	logs.DefaultErr.SetWriter(logs.Stdout)
+}
 
 func main() {
 	bar.Demo()
