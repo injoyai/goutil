611ce0a3b91134524e71dcf7ae02dee0d2aad5d3
钱纯净 <1113655791@qq.com>
2023-07-15 21:12:45 +0800

    增加http和times包

--- net/http/http_client.go
+++ net/http/http_client.go
@@ -14,26 +14,24 @@ var DefaultClient = NewClient()
 // NewClient
 // 新建HTTP请求客户端
 func NewClient() *Client {
-	data := &Client{Client: client()}
-	return data
-}
-
-func client() *http.Client {
-	return &http.Client{
-		Transport: &http.Transport{
-			//连接结束后会直接关闭,
-			//否则会加到连接池复用
-			DisableKeepAlives: true,
-			TLSClientConfig: &tls.Config{
-				//设置可以访问HTTPS
-				InsecureSkipVerify: true,
+	data := &Client{
+		Client: &http.Client{
+			Transport: &http.Transport{
+				//连接结束后会直接关闭,
+				//否则会加到连接池复用
+				DisableKeepAlives: true,
+				TLSClientConfig: &tls.Config{
+					//设置可以访问HTTPS
+					InsecureSkipVerify: true,
+				},
 			},
+			//设置连接超时时间,连接成功后无效
+			//连接成功后数据读取时间可以超过这个时间
+			//数据读取超时等可以nginx配置
+			Timeout: time.Second * 10,
 		},
-		//设置连接超时时间,连接成功后无效
-		//连接成功后数据读取时间可以超过这个时间
-		//数据读取超时等可以nginx配置
-		Timeout: time.Second * 10,
 	}
+	return data
 }
 
 type Client struct {
