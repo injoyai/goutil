2d478867720171c51b8f169b87c71ca605b50e00
injoy <1113655791@qq.com>
2023-07-18 17:26:02 +0800

    增加task

--- task/task_range.go
+++ task/task_range.go
@@ -2,37 +2,43 @@ package task
 
 import (
 	"github.com/injoyai/base/chans"
+	"github.com/injoyai/conv"
 	"github.com/injoyai/goutil/g"
 )
 
 type Runner func() error
 
-func NewOnce(retryNum, limit int) *Once {
-	return &Once{
-		RetryNum: retryNum,
-		Limit:    limit,
+func NewRange(limit int, retry ...int) *Range {
+	return &Range{
+		Retry: conv.GetDefaultInt(3, retry...),
+		Limit: limit,
 	}
 }
 
-type Once struct {
-	RetryNum int                    //重试次数
+type Range struct {
+	Retry    int                    //重试次数
 	Limit    int                    //并发数量
 	DoneFunc func(i int, err error) //子项执行完成
 }
 
-func (this *Once) SetDoneFunc(f func(i int, err error)) *Once {
+func (this *Range) SetRetry(retry int) *Range {
+	this.Retry = retry
+	return this
+}
+
+func (this *Range) SetDoneFunc(f func(i int, err error)) *Range {
 	this.DoneFunc = f
 	return this
 }
 
-func (this *Once) Run(list []Runner) []error {
+func (this *Range) Run(list []Runner) []error {
 	errList := []error(nil)
 	limit := chans.NewWaitLimit(uint(this.Limit))
 	for i, f := range list {
 		limit.Add()
 		go func(i int, errList []error) {
 			defer limit.Done()
-			err := g.Retry(f, this.RetryNum)
+			err := g.Retry(f, this.Retry)
 			if err != nil {
 				errList = append(errList, err)
 			}
