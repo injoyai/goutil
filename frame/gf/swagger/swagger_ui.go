c9a40fe4ac966dd1ee936ed81c00553de575fafc
injoy <1113655791@qq.com>
2023-12-18 09:55:40 +0800

    修改swagger js地址,原地址已不能使用

--- frame/gf/swagger/swagger_ui.go
+++ frame/gf/swagger/swagger_ui.go
@@ -2,25 +2,23 @@ package swagger
 
 var (
 	ui = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="description" content="SwaggerUI"/>
-  <title>SwaggerUI</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@latest/swagger-ui.css" />
-</head>
-<body>
-<div id="swagger-ui"></div>
-<script src="https://unpkg.com/swagger-ui-dist@latest/swagger-ui-bundle.js" crossorigin></script>
-<script>
-	window.onload = () => {
-		window.ui = SwaggerUIBundle({
-			url:    '%s',
-			dom_id: '#swagger-ui',
-		});
-	};
-</script>
-</body>
-</html>`
+        <html>
+          <head>
+            <title>SwaggerUI</title>
+            <!-- needed for adaptive design -->
+            <meta charset="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+            <style>
+              body {
+                margin: 0;
+                padding: 0;
+              }
+            </style>
+          </head>
+          <body>
+            <redoc spec-url='%s'></redoc>
+            <script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"> </script>
+          </body>
+        </html>`
 )
