7036c486995bbc50c8f7900d1aca9a481b1085e7
injoyai <1113655791@qq.com>
2024-03-29 14:12:03 +0800

    优化minio上传

--- other/upload/upload_minio.go
+++ other/upload/upload_minio.go
@@ -5,10 +5,23 @@ import (
 	"github.com/injoyai/base/bytes/crypt/md5"
 	"github.com/minio/minio-go"
 	"io"
+	"strings"
 )
 
 func NewMinio(cfg *MinioConfig) (Interface, error) {
-	cli, err := minio.New(cfg.Endpoint, cfg.AccessKey, cfg.SecretKey, false)
+	secure := false
+	endpoint := cfg.Endpoint
+	switch {
+	case strings.HasPrefix(cfg.Endpoint, "https://"):
+		secure = true
+		endpoint = strings.TrimPrefix(cfg.Endpoint, "https://")
+	case strings.HasPrefix(cfg.Endpoint, "http://"):
+		endpoint = strings.TrimPrefix(cfg.Endpoint, "http://")
+	default:
+		endpoint = cfg.Endpoint
+		cfg.Endpoint = "http://" + cfg.Endpoint
+	}
+	cli, err := minio.New(endpoint, cfg.AccessKey, cfg.SecretKey, secure)
 	if err != nil {
 		return nil, err
 	}
@@ -16,11 +29,11 @@ func NewMinio(cfg *MinioConfig) (Interface, error) {
 }
 
 type MinioConfig struct {
-	Endpoint   string //地址
+	Endpoint   string //地址 http://127.0.0.1:9000
 	AccessKey  string //访问key
 	SecretKey  string //秘钥
 	BucketName string //桶名称
-	Rename     bool   //重命名
+	Rename     bool   //是否重命名文件
 }
 
 type Minio struct {
