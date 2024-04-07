4d7c481c97f47db6d2a1a9a40f42d9dfbe815a0b
injoyai <1113655791@qq.com>
2024-04-07 16:37:58 +0800

    优化

--- net/ip/ip_func.go
+++ net/ip/ip_func.go
@@ -39,3 +39,37 @@ func ParseV4(s string) (net.IP, uint16, error) {
 	}
 	return ip, uint16(port), nil
 }
+
+func RangeV4(network string, fn func(n net.Interface, ip net.IP) bool) error {
+	is, err := net.Interfaces()
+	if err != nil {
+		return err
+	}
+	for _, v := range is {
+
+		if v.Flags&(1<<net.FlagLoopback) == 1 || v.Flags&(1<<net.FlagUp) == 0 {
+			continue
+		}
+		if len(network) > 0 && network != "all" && !strings.Contains(v.Name, network) {
+			continue
+		}
+
+		addrs, err := v.Addrs()
+		if err != nil {
+			return err
+		}
+
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+				ipv4 := ipnet.IP.To4()[:3]
+				for i := conv.Uint32([]byte{ipv4[0], ipv4[1], ipv4[2], 0}); i <= conv.Uint32([]byte{ipv4[0], ipv4[1], ipv4[2], 255}); i++ {
+					ip := net.IP(conv.Bytes(i))
+					if !fn(v, ip) {
+						break
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
