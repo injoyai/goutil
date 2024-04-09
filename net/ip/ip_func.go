60d8c77cb79eabeb7343a0b00df4336b81abfbfb
injoyai <1113655791@qq.com>
2024-04-09 08:52:31 +0800

    优化

--- net/ip/ip_func.go
+++ net/ip/ip_func.go
@@ -2,21 +2,22 @@ package ip
 
 import (
 	"errors"
+	"fmt"
 	"github.com/injoyai/conv"
 	"net"
 	"strconv"
 	"strings"
 )
 
-// RangeFunc 遍历ip地址
-func RangeFunc(start, end net.IP, f func(ip net.IP) bool) {
-	for i := conv.Uint32([]byte(start.To4())); i <= conv.Uint32([]byte(end.To4())); i++ {
-		ip := net.IP(conv.Bytes(i))
-		if !f(ip) {
-			break
-		}
-	}
-}
+//// RangeFunc 遍历ip地址
+//func RangeFunc(start, end net.IP, f func(ip net.IP) bool) {
+//	for i := conv.Uint32([]byte(start.To4())); i <= conv.Uint32([]byte(end.To4())); i++ {
+//		ip := net.IP(conv.Bytes(i))
+//		if !f(ip) {
+//			break
+//		}
+//	}
+//}
 
 func ParseV4(s string) (net.IP, uint16, error) {
 	host, portStr, err := net.SplitHostPort(s)
@@ -40,35 +41,57 @@ func ParseV4(s string) (net.IP, uint16, error) {
 	return ip, uint16(port), nil
 }
 
-func RangeV4(search string, fn func(n net.Interface, ip net.IP, self bool) bool) error {
-	is, err := net.Interfaces()
+func RangeNetwork(network string, fn func(inter *Inter)) error {
+	inters, err := net.Interfaces()
 	if err != nil {
 		return err
 	}
-	for _, network := range is {
-
-		if network.Flags&(1<<net.FlagLoopback) == 1 || network.Flags&(1<<net.FlagUp) == 0 {
+	for i, inter := range inters {
+		if inter.Flags&(1<<net.FlagLoopback) == 1 || inter.Flags&(1<<net.FlagUp) == 0 {
 			continue
 		}
-		if len(search) > 0 && search != "all" && !strings.Contains(network.Name, search) {
+		if len(network) > 0 && !strings.Contains(inter.Name, network) {
 			continue
 		}
+		fn(&Inter{
+			Index:     i,
+			Interface: inter,
+		})
+	}
+	return nil
+}
 
-		addrs, err := network.Addrs()
-		if err != nil {
-			return err
-		}
+type Inter struct {
+	Index int
+	net.Interface
+}
 
-		for _, addr := range addrs {
-			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
-				selfIP := ipnet.IP.To4()
-				for i := conv.Uint32([]byte{selfIP[0], selfIP[1], selfIP[2], 0}); i <= conv.Uint32([]byte{selfIP[0], selfIP[1], selfIP[2], 255}); i++ {
-					ip := net.IP(conv.Bytes(i))
-					if !fn(network, ip, selfIP.String() == ip.String()) {
-						break
-					}
-				}
+func (this *Inter) Print() {
+	fmt.Printf("\n%d: %s (%s):\n", this.Index, this.Name, this.HardwareAddr)
+}
+
+// RangeSegment 遍历网段
+func (this *Inter) RangeSegment(fn func(ipv4 net.IP, self bool) bool) error {
+	return this.RangeV4(func(ipv4 net.IP) bool {
+		for i := conv.Uint32([]byte{ipv4[0], ipv4[1], ipv4[2], 0}); i <= conv.Uint32([]byte{ipv4[0], ipv4[1], ipv4[2], 255}); i++ {
+			ip := net.IP(conv.Bytes(i))
+			if !fn(ip, ip.String() == ipv4.String()) {
+				return false
 			}
+		}
+		return true
+	})
+}
+
+// RangeV4 遍历ipv4
+func (this *Inter) RangeV4(fn func(ipv4 net.IP) bool) error {
+	addrs, err := this.Addrs()
+	if err != nil {
+		return err
+	}
+	for _, addr := range addrs {
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+			fn(ipNet.IP.To4())
 		}
 	}
 	return nil
