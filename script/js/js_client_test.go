94921454fc73d814c4c9ef00573d32d8d1145d79
injoyai <1113655791@qq.com>
2025-01-15 14:53:53 +0800

    增加测试用例

--- script/js/js_client_test.go
+++ script/js/js_client_test.go
@@ -2,6 +2,7 @@ package js
 
 import (
 	"errors"
+	"fmt"
 	"github.com/injoyai/goutil/script"
 	"testing"
 )
@@ -22,4 +23,29 @@ func TestNew(t *testing.T) {
 	t.Log(n.Exec(`100`))
 	t.Log(n.Exec(`"test"`))
 
+}
+
+func TestObj(t *testing.T) {
+	x := New()
+	err := x.Set("obj", &Obj{Name: "test", Age: 18})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	x.Exec(`print(obj.Name,obj.Age)`)
+	_, err = x.Exec(`obj.Print()`)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+type Obj struct {
+	Name string
+	Age  int
+}
+
+func (this *Obj) Print() {
+	fmt.Println(this.Name, this.Age)
 }
