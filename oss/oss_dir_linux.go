674df16f8e2655942045a1b6b7e15d6d43a1c4f3
injoyai <1113655791@qq.com>
2025-08-29 14:04:42 +0800

    优化

--- oss/oss_dir_linux.go
+++ oss/oss_dir_linux.go
@@ -6,7 +6,7 @@ import (
 
 // UserDataDir 系统用户数据路径,AppData/Local
 func UserDataDir(join ...string) string {
-	return filepath.Join(append([]string{"/var/lib/injoy/"}, join...)...)
+	return filepath.Join(append([]string{"/var/lib/"}, join...)...)
 }
 
 // UserStartupDir 自启路径,这个对linux没啥用,需要编辑文件,而不是放到目录就行
