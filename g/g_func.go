33ee38a07a94862e5e55a6bb92c3ced505192892
injoyai <1113655791@qq.com>
2025-08-29 11:34:14 +0800

    优化

--- g/g_func.go
+++ g/g_func.go
@@ -20,29 +20,36 @@ import (
 
 //========================================Chan========================================
 
+///*
+//Iter
+//Range 仿python的range 参数1-3个
+//
+//当1个参数, 例 Range(5) 输出 0,1,2,3,4
+//当2个参数, 例 Range(1,5) 输出 1,2,3,4
+//当3个参数, 例 Range(0,5,2) 输出 0,2,4
+//*/
+//func Iter[T types.Integer](n T, m ...T) iter.Seq[T] {
+//	return func(f func(T) bool) {
+//		start, end, step := T(0), n, T(1)
+//		switch len(m) {
+//		case 0:
+//		case 1:
+//			start, end = n, m[0]
+//		default:
+//			start, end, step = n, m[0], m[1]
+//		}
+//		for i := start; i <= end; i += step {
+//			f(i)
+//		}
+//	}
+//}
+
 /*
 Range 仿python的range 参数1-3个
 
-	当1个参数, 例 Range(5) 输出 0,1,2,3,4
-	当2个参数, 例 Range(1,5) 输出 1,2,3,4
-	当3个参数, 例 Range(0,5,2) 输出 0,2,4
-
-	todo go1.23版本替换
-	func Range[T conv.Integer](n T, m ...T) iter.Seq[T] {
-		return func(f func(T) bool) {
-			start, end, step := T(0), n, T(1)
-			switch len(m) {
-			case 0:
-			case 1:
-				start, end = n, m[0]
-			default:
-				start, end, step = n, m[0], m[1]
-			}
-			for i := start; i < end; i += step {
-				f(i)
-			}
-		}
-	}
+当1个参数, 例 Range(5) 输出 0,1,2,3,4
+当2个参数, 例 Range(1,5) 输出 1,2,3,4
+当3个参数, 例 Range(0,5,2) 输出 0,2,4
 */
 func Range(n int, m ...int) <-chan int {
 	return chans.Range(n, m...)
